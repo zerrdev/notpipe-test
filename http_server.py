26cfe31993c737e8b35b2abc371eb03c63e29607
zerrdev <gabriel@zerr.dev>
2025-01-22 21:18:43 +0000

    test 19

--- http_server.py
+++ http_server.py
@@ -6,7 +6,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'19')
+            self.wfile.write(b'20')
         else:
             self.send_response(404)
             self.end_headers()
