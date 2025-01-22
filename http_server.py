5adfcbe7f491a1ddcc2aaa89eca3c4380cac1bdc
zerrdev <gabriel@zerr.dev>
2025-01-22 10:31:45 +0000

    test commit 10

--- http_server.py
+++ http_server.py
@@ -6,7 +6,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'11')
+            self.wfile.write(b'12')
         else:
             self.send_response(404)
             self.end_headers()
